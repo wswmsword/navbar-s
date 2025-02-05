1bc050b3e6038dbd3a34a89475f9cbf65f2be973
wswmsword <cjzjzzz@gmail.com>
2025-02-05 09:45:53 +0800

    update example

--- examples/dark-space/app/[[...lng]]/body.js
+++ examples/dark-space/app/[[...lng]]/body.js
@@ -1,12 +1,17 @@
 "use client"
 
-import { createContext, useState } from "react";
+import { createContext, useLayoutEffect, useState } from "react";
 
 export const ThemeContext = createContext();
 
 export default function Body({ children }) {
 
   const [theme, setTm] = useState(null);
+
+  useLayoutEffect(() => {
+    // avoid reset theme when changing language
+    setTm(document.body.classList.add(window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"));
+  }, []);
 
   return <body className={theme}><ThemeContext.Provider value={setTm}>{children}</ThemeContext.Provider></body>;
 }
